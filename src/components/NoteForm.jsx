96d0cc3de0133c7aed95b32781ba4240a9e9e130
Daris Bouaziz <mkdarice@MacBookAir-de-Daris.local>
2026-02-02 11:47:15 -0500

    Form Data Object

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 
 const NoteForm = () => {
-  const [title, setTitle] = useState("");
-  const [priority, setPriority] = useState("Medium");
-  const [category, setCategory] = useState("Work");
-  const [description, setDescription] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    priority: "Medium",
+    category: "Work",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   return (
     <form className="mb-6">
@@ -14,10 +20,11 @@ const NoteForm = () => {
           Title
         </label>
         <input
+          name="title"
           type="text"
           className="w-full pd-2 border rounded-lg"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={formData.title}
+          onChange={handleChange}
         />
       </div>
       {/* Priority */}
@@ -26,10 +33,11 @@ const NoteForm = () => {
           Priority
         </label>
         <select
+          name="priority"
           type="text"
           className="w-full pd-2 border rounded-lg"
-          value={priority}
-          onChange={(e) => setPriority(e.target.value)}
+          value={formData.priority}
+          onChange={handleChange}
         >
           <option value="High">🔴 High</option>
           <option value="Medium">🟠 Medium</option>
@@ -42,10 +50,11 @@ const NoteForm = () => {
           Category
         </label>
         <select
+          name="category"
           type="text"
           className="w-full pd-2 border rounded-lg"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
+          value={formData.category}
+          onChange={handleChange}
         >
           <option value="Work">📂 Work</option>
           <option value="Personal">🏠 Personal</option>
@@ -58,10 +67,11 @@ const NoteForm = () => {
           Description
         </label>
         <textarea
+          name="description"
           type="text"
           className="w-full pd-2 border rounded-lg"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          value={formData.description}
+          onChange={handleChange}
         ></textarea>
       </div>
       <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover: bg-purple-600">
