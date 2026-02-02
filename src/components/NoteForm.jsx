1fac845a2ef52a2f6478c64db98ac7ab09b3c124
Daris Bouaziz <mkdarice@MacBookAir-de-Daris.local>
2026-02-02 12:56:01 -0500

    Form Submission & Global State

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const NoteForm = () => {
+const NoteForm = ({ notes, setNotes }) => {
   const [formData, setFormData] = useState({
     title: "",
     priority: "Medium",
@@ -12,8 +12,29 @@ const NoteForm = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation
+    if (!formData.title || !formData.description) return;
+
+    // Create note object
+    const newNote = { id: Date.now(), ...formData };
+
+    //Add notes to state
+    setNotes([newNote, ...notes]);
+
+    //Reset form data
+    setFormData({
+      title: "",
+      priority: "Medium",
+      category: "Work",
+      description: "",
+    });
+  };
+
   return (
-    <form className="mb-6">
+    <form onSubmit={handleSubmit} className="mb-6">
       {/* Title */}
       <div className="mb-4">
         <label htmlFor="title" className="block font-semibold">
